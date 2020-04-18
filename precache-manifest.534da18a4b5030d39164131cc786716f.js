330a9c3b2c7312af9e6b6cd8b92ff661f1e65d98
Yannic Fréson <yannic.freson1@gmail.com>
2020-04-18 16:02:18 +0200

    Updates

---
+++ precache-manifest.534da18a4b5030d39164131cc786716f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de62de1aade817df6826fdc58ed6c68a",
+    "url": "/hexclock/index.html"
+  },
+  {
+    "revision": "c37c09943b2c74386cf4",
+    "url": "/hexclock/static/css/main.013f8b1b.chunk.css"
+  },
+  {
+    "revision": "b3f9034ab0033179abb2",
+    "url": "/hexclock/static/js/2.aeab60a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hexclock/static/js/2.aeab60a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37c09943b2c74386cf4",
+    "url": "/hexclock/static/js/main.20137ea6.chunk.js"
+  },
+  {
+    "revision": "159bfcd9df2ef0f8220d",
+    "url": "/hexclock/static/js/runtime-main.f51757a1.js"
+  }
+]);
